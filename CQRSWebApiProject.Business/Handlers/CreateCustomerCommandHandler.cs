4a75e543ab9bd2b87be6487c77c73b88be4302c2
Egehan Çakır <e.cakir@baylanwatermeters.com>
2025-02-28 15:05:26 +0300

    Auto mapper

---
+++ CQRSWebApiProject.Business/Handlers/CreateCustomerCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRSWebApiProject.Business.Commands;
+using CQRSWebApiProject.Business.DTO.Response;
+using CQRSWebApiProject.DAL.Concrete.EntityFramework.GenericRepository;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSWebApiProject.Business.Handlers
+{
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, CreateCustomerResponse>
+    {
+        private readonly IGenericRepository<Entity.Concrete.Customer> repository;
+        private readonly IActionResultTypeMapper mapper;
+
+        public CreateCustomerCommandHandler(IGenericRepository<Entity.Concrete.Customer> , IActionResultTypeMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+    }
+}
--- CQRSWebApiProject.Business/Queries/GetCustomerByIDQuery.cs
+++ CQRSWebApiProject.Business/Queries/GetCustomerByIDQuery.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CQRSWebApiProject.Business.DTO.Response;
 using MediatR;
 
 namespace CQRSWebApiProject.Business.Queries
 {
-    public class GetCustomerByIDQuery : IRequest<Get>
+    public class GetCustomerByIDQuery : IRequest<GetCustomerByIdQueryResponse>
     {
+        public int Id { get; set; } 
+        public GetCustomerByIDQuery(int id)
+        {
+            id = id;
+        }
     }
 }
