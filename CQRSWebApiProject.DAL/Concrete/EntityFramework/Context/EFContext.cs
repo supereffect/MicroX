46157dbe937453d76202cf2094f175c9712c8f84
Egehan Çakır <e.cakir@baylanwatermeters.com>
2025-02-28 15:05:04 +0300

    EF core eklendi

---
+++ CQRSWebApiProject.DAL/Concrete/EntityFramework/Context/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CQRSWebApiProject.DAL.Concrete.EntityFramework.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Entity.Concrete.Customer> Customers { get; set; }
+
+        public DbSet<Entity.Concrete.Address> Addresses { get; set; }
+    }
+}
---
+++ CQRSWebApiProject.DAL/Concrete/EntityFramework/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using CQRSWebApiProject.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CQRSWebApiProject.DAL.Concrete.EntityFramework.GenericRepository
+{
+    public interface IGenericRepository<T> where T : BaseEntity, IEntity, new()
+    {
+        Task<T> Get(Expression<Func<T, bool>> expression);
+
+        Task<List<T>> GetList(Expression<Func<T, bool>> expression = null);
+
+        Task<T> Add(T entity);
+
+        Task<T> Update(T entity);
+
+        void Delete(T entity);
+
+        Task<int> SaveChangesAsync();
+
+        IEnumerable<T> GetQueryable();
+    }
+}
