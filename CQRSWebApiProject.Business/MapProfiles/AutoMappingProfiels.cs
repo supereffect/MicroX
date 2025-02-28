a07cef5d55d497ed8819b32c8cbfbeccfe47beab
Egehan Çakır <e.cakir@baylanwatermeters.com>
2025-02-28 16:54:30 +0300

    oops eksik yolcu uçtum

--- CQRSWebApiProject.Business/MapProfiles/AutoMappingProfiels.cs
+++ CQRSWebApiProject.Business/MapProfiles/AutoMappingProfiels.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CQRSWebApiProject.Business.DTO.Request;
+using CQRSWebApiProject.Business.DTO.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace CQRSWebApiProject.Business.MapProfiles
         {
 
             CreateMap<Entity.Concrete.Customer, CreateCustomerRequest>().ReverseMap();
-
+            CreateMap<Entity.Concrete.Customer, CreateCustomerResponse>().ReverseMap();
             ////kural belirtmek isterilirse aşağıdaki gibi eklenebilir diye burayı koyuyorum ilgilenenler için<
             //   CreateMap<GrpcPlatformModel, Platform>().ForMember(x => x.ExternalId, opt => opt.MapFrom(src => src.PlatformId))// except externalıd=>platformid mapping are not necessary but, its just for trus issues :):):
             //       .ForMember(x => x.Name, opt => opt.MapFrom(src => src.Name))
